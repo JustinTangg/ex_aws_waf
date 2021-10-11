c2b7c504f27904e57e703e6892e883837bf346f9
Justin Tang <justin.tang@vonage.com>
2021-10-11 09:46:57 -0400

    init

---
+++ lib/ex_aws/wafv2.ex
@@ -0,0 +1,18 @@
+defmodule ExAws.Wafv2 do
+  @moduledoc """
+  Documentation for `ExAws.Wafv2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAws.Wafv2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aws/wafv2_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAws.Wafv2Test do
+  use ExUnit.Case
+  doctest ExAws.Wafv2
+
+  test "greets the world" do
+    assert ExAws.Wafv2.hello() == :world
+  end
+end
